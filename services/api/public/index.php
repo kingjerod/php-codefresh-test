79689f539b949b413abbf5da0d4ad9620a9ac391
Jerod King <jerodking@gmail.com>
2017-04-19 15:28:39 -0700

    First commit.

---
+++ services/api/public/index.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+PHP_Timer::start();
+echo "This is the the API page! ";
+echo PHP_Timer::resourceUsage();
---
+++ services/portal/public/index.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+PHP_Timer::start();
+echo "This is the portal page. ";
+echo PHP_Timer::resourceUsage();
