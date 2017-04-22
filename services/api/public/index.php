60df8708988a6e285e2235ac16648bc236485c21
Jerod King <jerodking@gmail.com>
2017-04-22 15:32:19 -0700

    Testing out env stuff.

--- services/api/public/index.php
+++ services/api/public/index.php
@@ -1,5 +1,13 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+
 PHP_Timer::start();
-echo "This is the the API page! ";
+echo "This is the the API page!<br />";
+echo "DB_HOST: " . getenv('DB_HOST') . "<br />";
+echo "DB_DATABASE: " . getenv('DB_DATABASE') . "<br />";
+echo "DB_USERNAME: " . getenv('DB_USERNAME') . "<br />";
 echo PHP_Timer::resourceUsage();
