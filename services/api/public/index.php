3c0822c8a9aa12b22224333fa90909cc7601649a
Jerod King <jerodking@gmail.com>
2017-04-22 16:42:14 -0700

    Moving volume line after composer install.

--- services/api/public/index.php
+++ services/api/public/index.php
@@ -7,29 +7,27 @@ try {
 }
 
 try {
-    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
     $dotenv->load();
 } catch (\Exception $ex) {
     echo "Did not load .env file. This is ok in production.<br />";
 }
 
-echo "Work dangit!";
-//
-//try {
-//    PHP_Timer::start();
-//    echo "This is the the API page!<br />";
-//
-//    $vars = ['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'ASS'];
-//
-//    foreach($vars as $var) {
-//        if (getenv($var)) {
-//            echo $var . ": " . getenv($var) . "<br />";
-//        } else {
-//            echo "No " . $var . " env var set.<br />";
-//        }
-//    }
-//
-//    echo PHP_Timer::resourceUsage();
-//} catch (\Exception $ex) {
-//    echo "PHP exception: " . $ex->getMessage();
-//}
+try {
+    PHP_Timer::start();
+    echo "This is the the API page!<br />";
+
+    $vars = ['DB_HOST', 'DB_DATABASE', 'DB_USERNAME'];
+
+    foreach($vars as $var) {
+        if (getenv($var)) {
+            echo $var . ": " . getenv($var) . "<br />";
+        } else {
+            echo "No " . $var . " env var set.<br />";
+        }
+    }
+
+    echo PHP_Timer::resourceUsage();
+} catch (\Exception $ex) {
+    echo "PHP exception: " . $ex->getMessage();
+}
