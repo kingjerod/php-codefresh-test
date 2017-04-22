22f12cf7a7718b121bb5488dcbe898bb7f0419dd
Jerod King <jerodking@gmail.com>
2017-04-22 15:53:37 -0700

    Changing api back to basic.

--- services/api/public/index.php
+++ services/api/public/index.php
@@ -1,33 +1,34 @@
 <?php
-
-try {
-    require __DIR__ . '/../vendor/autoload.php';
-} catch (\Exception $ex) {
-    die('Failed to load autoload.php file.');
-}
-
-try {
-    $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
-    $dotenv->load();
-} catch (\Exception $ex) {
-    echo "Did not load .env file. This is ok in production.<br />";
-}
-
-try {
-    PHP_Timer::start();
-    echo "This is the the API page!<br />";
-
-    $vars = ['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'ASS'];
-
-    foreach($vars as $var) {
-        if (getenv($var)) {
-            echo $var . ": " . getenv($var) . "<br />";
-        } else {
-            echo "No " . $var . " env var set.<br />";
-        }
-    }
-
-    echo PHP_Timer::resourceUsage();
-} catch (\Exception $ex) {
-    echo "PHP exception: " . $ex->getMessage();
-}
+echo "Why u no work.";
+//
+//try {
+//    require __DIR__ . '/../vendor/autoload.php';
+//} catch (\Exception $ex) {
+//    die('Failed to load autoload.php file.');
+//}
+//
+//try {
+//    $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+//    $dotenv->load();
+//} catch (\Exception $ex) {
+//    echo "Did not load .env file. This is ok in production.<br />";
+//}
+//
+//try {
+//    PHP_Timer::start();
+//    echo "This is the the API page!<br />";
+//
+//    $vars = ['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'ASS'];
+//
+//    foreach($vars as $var) {
+//        if (getenv($var)) {
+//            echo $var . ": " . getenv($var) . "<br />";
+//        } else {
+//            echo "No " . $var . " env var set.<br />";
+//        }
+//    }
+//
+//    echo PHP_Timer::resourceUsage();
+//} catch (\Exception $ex) {
+//    echo "PHP exception: " . $ex->getMessage();
+//}
